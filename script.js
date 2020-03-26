443f8fd95557b08ae7a14ab99bc0081b028cba70
Steven Kawafuchi <skawafuchi@gmail.com>
2020-03-26 11:12:38 -0700

    Added dice animation

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-function rollDice() {
-  var randomNumberPlayerOne = Math.ceil(Math.random() * 6)
-  var randomNumberPlayerTwo = Math.ceil(Math.random() * 6)
-  var title = document.querySelector("h1");
-  document.getElementById("playerOne").setAttribute("src", "images/dice" + randomNumberPlayerOne + ".png");
-  document.getElementById("playerTwo").setAttribute("src", "images/dice" + randomNumberPlayerTwo + ".png");
-  if (randomNumberPlayerOne > randomNumberPlayerTwo) {
-    title.textContent = "🚩Player 1 wins!"
-  } else if (randomNumberPlayerOne < randomNumberPlayerTwo) {
-    title.textContent = "Player 2 wins!🚩"
-  } else {
-    title.textContent = "🚩Draw🚩"
-  }
+var diceValue1;
+var diceValue2;
+
+function randomizeDice() {
+  diceValue1 = Math.ceil(Math.random() * 6);
+  diceValue2 = Math.ceil(Math.random() * 6);
+  document.getElementById("playerOne").setAttribute("src", "images/dice" + diceValue1 + ".png");
+  document.getElementById("playerTwo").setAttribute("src", "images/dice" + diceValue2 + ".png");
 }
+
+function rollDice() {
+  let animation = setInterval(randomizeDice, 50);
+  setTimeout(function() {
+    clearInterval(animation);
+    let title = document.querySelector("h1");
+    if (diceValue1 > diceValue2) {
+      title.textContent = "🚩Player 1 wins!";
+    } else if (diceValue1 < diceValue2) {
+      title.textContent = "Player 2 wins!🚩";
+    } else {
+      title.textContent = "🚩Draw🚩";
+    }
+  }, 300);
+}
+
+document.querySelector(".button").addEventListener("click", rollDice);
